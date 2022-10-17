9dfcaa1f90079073874f98759c83461dbc18f192
Arczius <70692175+Arczius@users.noreply.github.com>
2022-10-17 18:30:52 +0200

    built a seeder to import all the songs to the database

---
+++ htdocs/app/Database/Seeds/SongsSeeder.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class SongsSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'song_name' => 'POLTERGEIST!',
+                'artist_name' => 'CORPSE',
+            ],
+            [
+                'song_name' => 'Devil Town',
+                'artist_name' => 'Cavetown',
+            ],
+            [
+                'song_name' => 'Stan',
+                'artist_name' => 'Eminem',
+            ],
+            [
+                'song_name' => 'Rät',
+                'artist_name' => 'Penelope Scott',
+            ],
+            [
+                'song_name' => 'fuk u lol',
+                'artist_name' => 'CORPSE',
+            ],
+            [
+                'song_name' => 'Siren Head',
+                'artist_name' => 'Chills',
+            ],
+            [
+                'song_name' => 'White Tee',
+                'artist_name' => 'CORPSE',
+            ],
+            [
+                'song_name' => 'Lemon Boy',
+                'artist_name' => 'Cavetown',
+            ],
+            [
+                'song_name' => 'The Search',
+                'artist_name' => 'NF',
+            ],
+            [
+                'song_name' => '7 Years (Remix)',
+                'artist_name' => 'Sik World',
+            ],
+            [
+                'song_name' => 'Art Is Dead',
+                'artist_name' => 'Bo Burnham',
+            ],
+            [
+                'song_name' => 'Boys Will Be Bugs',
+                'artist_name' => 'Cavetown',
+            ],
+            [
+                'song_name' => 'Sarcasm',
+                'artist_name' => 'Get Scared',
+            ],
+            [
+                'song_name' => "Wolf in Sheeps Clothing",
+                'artist_name' => 'Set It Off',
+            ],
+            [
+                'song_name' => 'Partners in Crime',
+                'artist_name' => 'Set It Off',
+            ],
+            [
+                'song_name' => 'Surface Pressure',
+                'artist_name' => 'Jessica Darrow',
+            ],
+            [
+                'song_name' => 'Boulevard of Broken Dreams',
+                'artist_name' => 'Green Day',
+            ],
+            [
+                'song_name' => 'Monster',
+                'artist_name' => 'Skillet',
+            ],
+            [
+                'song_name' => 'House of Memories',
+                'artist_name' => 'Panic! At The Disco',
+            ],
+            [
+                'song_name' => 'Bring Me To Life',
+                'artist_name' => 'Evanescence',
+            ],
+            [
+                'song_name' => 'The Pretender',
+                'artist_name' => 'Foo Fighters',
+            ],
+            [
+                'song_name' => 'Never Satisfied',
+                'artist_name' => 'CORPSE',
+            ],
+            [
+                'song_name' => "Scotty Doesn't Know",
+                'artist_name' => 'Lustra',
+            ],
+            [
+                'song_name' => 'All The Small Things',
+                'artist_name' => 'blink-182',
+            ],
+            [
+                'song_name' => 'Popular Monster',
+                'artist_name' => 'Falling In Reverse',
+            ],
+            [
+                'song_name' => 'Everywhere I Go',
+                'artist_name' => 'Hollywood Undead',
+            ],
+            [
+                'song_name' => 'Throne',
+                'artist_name' => 'Bring Me The Horizon',
+            ],
+            [
+                'song_name' => 'Tone Deaf',
+                'artist_name' => 'Eminem',
+            ],
+            [
+                'song_name' => 'Without Me',
+                'artist_name' => 'Eminem',
+            ],
+            [
+                'song_name' => 'The Real Slim Shady',
+                'artist_name' => 'Eminem',
+            ],
+            [
+                'song_name' => 'Dear Maria, Count Me In',
+                'artist_name' => 'All Time Low',
+            ],
+            [
+                'song_name' => 'Animal I Have Become',
+                'artist_name' => 'Three Days Grace',
+            ],
+            [
+                'song_name' => 'STUPID',
+                'artist_name' => 'Ashnikko',
+            ],
+            [
+                'song_name' => 'Aim Steady',
+                'artist_name' => 'Zero 9:36',
+            ],
+            [
+                'song_name' => 'Daggers',
+                'artist_name' => 'Zero 9:36',
+            ],
+            [
+                'song_name' => 'The Drug In Me Is You',
+                'artist_name' => 'Falling In Reverse',
+            ],
+            [
+                'song_name' => 'Can You Feel My Heart',
+                'artist_name' => 'Bring Me The Horizon',
+            ],
+            [
+                'song_name' => 'Blood // Water',
+                'artist_name' => 'grandson',
+            ],
+            [
+                'song_name' => 'The Rumbling',
+                'artist_name' => 'SiM',
+            ],
+            [
+                'song_name' => 'Juliet',
+                'artist_name' => 'Cavetown',
+            ],
+            [
+                'song_name' => 'Home',
+                'artist_name' => 'Cavetown',
+            ],
+            [
+                'song_name' => 'Cradles',
+                'artist_name' => 'Sub Urban',
+            ],
+            [
+                'song_name' => 'overwhelmed',
+                'artist_name' => 'Royal & the serpent',
+            ],
+            [
+                'song_name' => "I'm Yer Dad",
+                'artist_name' => 'GRLwood',
+            ],
+            [
+                'song_name' => 'Fairytale',
+                'artist_name' => 'Alexander Rybak',
+            ],
+            [
+                'song_name' => 'Shinzo wo Sasageyo!',
+                'artist_name' => 'Linked Horizon',
+            ],
+            [
+                'song_name' => 'life waster',
+                'artist_name' => 'CORPSE',
+            ],
+            [
+                'song_name' => 'Let You Down',
+                'artist_name' => 'NF',
+            ],
+            [
+                'song_name' => 'Teenage Dirtbag',
+                'artist_name' => 'Cavetown',
+            ],
+            [
+                'song_name' => 'Pumped Up Kicks',
+                'artist_name' => 'Foster The People',
+            ],
+            [
+                'song_name' => 'Wachtmuziek (Sped Up)',
+                'artist_name' => 'Joost',
+            ],
+            [
+                'song_name' => 'Riot',
+                'artist_name' => 'Hollywood Undead',
+            ],
+            [
+                'song_name' => "I'll Sleep When I'm Dead",
+                'artist_name' => 'Set It Off',
+            ],
+            [
+                'song_name' => 'Arms Tonite',
+                'artist_name' => 'Mother Mother',
+            ],
+            [
+                'song_name' => 'Strike It Out',
+                'artist_name' => 'MIYAVI',
+            ],
+            [
+                'song_name' => "What's New Scooby-Doo",
+                'artist_name' => 'Simple Plan',
+            ],
+            [
+                'song_name' => 'Fryslân Bop',
+                'artist_name' => 'Joost',
+            ],
+            [
+                'song_name' => 'Buddy Holly',
+                'artist_name' => 'Weezer',
+            ],
+            [
+                'song_name' => 'Kernkraft 400',
+                'artist_name' => 'Zombie Nation',
+            ],
+            [
+                'song_name' => 'Prom Queen',
+                'artist_name' => 'Beach Bunny',
+            ],
+        ];
+
+        foreach($data as $item){
+            $this->db->table('songs')->insert($item);
+        }
+    }
+}
